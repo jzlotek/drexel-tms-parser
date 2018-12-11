e6178df099808f745d49ff0aa01b0d611ef346cb
John Zlotek <jzlotek@gmail.com>
2018-12-11 12:44:51 -0500

    Move app.py

---
+++ src/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+import datetime
+import multiprocessing
+import os
+from crawler.tms_crawler import Crawler
+from db import database
+
+
+
+def run_crawler():
+    try:
+        crawler = Crawler()
+        crawler.crawl()
+    except Exception as e:
+        print('\n\n{}\n\n{}\n\n'.format(str(e), str(datetime.datetime.now())))
+
+
+app = Flask(__name__,
+            static_folder="../dist/static",
+            template_folder="../dist")
+
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return render_template("../dist/index.html")
+
+@app.route('/course/<path:path>', methods=['GET'])
+def get_course(path):
+    print(path)
+    database.execute()
+    return path
+
+
+if __name__ == '__main__':
+    database.crn(32008)
+    database.execute()
+    if 'dist' not in os.listdir('../'):
+        os.mkdir('../dist')
+
+    if '.tmp' not in os.listdir('../dist'):
+        os.mkdir('../dist/.tmp')
+
+
+    # crawler_process = multiprocessing.Process(target=run_crawler)
+
+    try:
+        # crawler_process.start()
+        app.run(debug=True)
+    except Exception as e:
+        print(e)
+        # crawler_process.terminate()
